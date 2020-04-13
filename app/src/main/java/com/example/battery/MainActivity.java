f924d9d8c46bab250cd54a6fec88ba2e5b43659e
Ronnyn011 <48674898+Ronny011@users.noreply.github.com>
2020-04-13 07:15:34 +0300

    Refactoring and new activity
    added activity for share button and changed package name from default to battery

---
+++ app/src/main/java/com/example/battery/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.battery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button share;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        share = (findViewById(R.id.button));//button element
+        share.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ShareActivity.class);
+        startActivity(intent);
+    }
+}
