a120a1695b90251ab1d43c61a217af78e8d632c2
Ronnyn011 <48674898+Ronny011@users.noreply.github.com>
2020-04-16 07:56:21 +0300

    finalization
    final version with full funcionality

--- app/src/main/java/com/example/battery/ShareActivity.java
+++ app/src/main/java/com/example/battery/ShareActivity.java
@@ -2,30 +2,39 @@ package com.example.battery;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
-    TextView status;//low/medium/high
-    ImageView icon;//emojis
-    int percentage;
-
+    private int relevant;//the current icon depending on percentage
+    int[] images = {R.drawable.open_hands, R.drawable.thumbs_up, R.drawable.thumbs_down};//emojis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
-        status = findViewById(R.id.status);
-        icon = findViewById(R.id.icon);
+        TextView status = findViewById(R.id.status);
+        ImageView icon = findViewById(R.id.icon);
         Intent intent = getIntent();
-        percentage = intent.getIntExtra("percentage", 0);
+        int percentage = intent.getIntExtra("percentage", 0);
         System.out.println(percentage);
-        if (percentage >= 1 && percentage <= 29)
-            status.setText("Low");
-        else if (percentage >= 30 && percentage <= 74)
-            status.setText("Medium");
-        else if (percentage >= 75 && percentage <= 100)
-            status.setText("High");
+        if (percentage >= 1 && percentage <= 29){
+            status.setText(R.string.low);
+            status.setTextColor(Color.RED);
+            status.setBackgroundColor(Color.GREEN);
+            icon.setImageResource(images[2]);}
+        else if (percentage >= 30 && percentage <= 74){
+            status.setText(R.string.medium);
+            status.setTextColor(Color.GREEN);
+            status.setBackgroundColor(Color.YELLOW);
+            icon.setImageResource(images[0]);}
+        else if (percentage >= 75 && percentage <= 100){
+            status.setText(R.string.high);
+            status.setTextColor(Color.BLUE);
+            status.setBackgroundColor(Color.GRAY);
+            icon.setImageResource(images[1]);}
     }
 }
+
 
