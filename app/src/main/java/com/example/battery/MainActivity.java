5c3e9639a975a2973ae96fd6462842d688802fa2
Ronnyn011 <48674898+Ronny011@users.noreply.github.com>
2020-04-21 16:39:59 +0300

    major update
    removed all button disable/enable and view watcher functionality. Wasn't playing nice with toasts

--- app/src/main/java/com/example/battery/MainActivity.java
+++ app/src/main/java/com/example/battery/MainActivity.java
@@ -15,10 +15,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static String regex_p = "^[+][0-9]{10,13}$";//for phone field
+    private static String regex_p = "^[+]?[0-9]{10,13}$";//for phone field
     private static String regex_n = ".*\\d+.*";// for name field
     private Button share;
-    private Button dummy;
     private TextView name;
     private TextView phone;
     private TextView battMessage;
@@ -30,74 +29,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         share = findViewById(R.id.button);//button element
-        dummy = findViewById(R.id.trans_button);
         battMessage = findViewById(R.id.battBox);
         name = findViewById(R.id.nameBox);
         phone = findViewById(R.id.phoneBox);
         share.setOnClickListener(this);
-        dummy.setOnClickListener(this);
         battBroadcast = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                pct = intent.getIntExtra("level",0);
+                pct = intent.getIntExtra("level", 0);
                 battMessage.setText("Battery percentage " + String.valueOf(pct) + "%");
             }
         };
-        name.addTextChangedListener(watcher);
-        phone.addTextChangedListener(watcher);
     }
-    private TextWatcher watcher = new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-        }
-
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String nameChange = name.getText().toString();
-            String phoneChange = phone.getText().toString();
-            if(nameChange.isEmpty() || phoneChange.isEmpty())
-            {
-                try {
-                    throw new Exception("All fields must be filled");
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    share.setEnabled(false);
-                }
-            }
-            else if (nameChange.matches(regex_n) || phoneChange.matches(regex_p))
-            {
-                try
-                {
-                    throw new Exception("Make sure you provide correct input");
-                }
-                catch (Exception e)
-                {
-                    Toast.makeText(getApplicationContext(),e.getMessage(), Toast.LENGTH_SHORT).show();
-                    share.setEnabled(false);
-                }
-            }
-            else if (nameChange.contains(" ") || phoneChange.contains(" "))
-            {
-                try
-                {
-                    throw new Exception("No spaces are allowed in the fields");
-                }
-                catch (Exception e)
-                {
-                    Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
-                    share.setEnabled(false);
-                }
-            }
-            else
-                share.setEnabled(true);//if everything is ok
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-        }
-    };
 
     @Override
     protected void onResume() {
@@ -105,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         registerReceiver(battBroadcast,
                 new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -113,10 +57,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.trans_button){
-
+        String nameChange = name.getText().toString();
+        String phoneChange = phone.getText().toString();
+        if (nameChange.isEmpty() || phoneChange.isEmpty()) {
+            try {
+                throw new Exception("All fields must be filled");
+            }
+            catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         }
-        else if (v.getId() == R.id.button){
+        else if (nameChange.matches(regex_n) || !phoneChange.matches(regex_p)) {
+            try {
+                throw new Exception("Make sure you provide correct input");
+            }
+            catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (nameChange.contains(" ") || phoneChange.contains(" ")) {
+            try {
+                throw new Exception("No spaces are allowed in the fields");
+            }
+            catch (Exception e) {
+                Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
             Intent share = new Intent(this, ShareActivity.class);
             share.putExtra("percentage", pct);
             startActivity(share);//goes to second activity on click
